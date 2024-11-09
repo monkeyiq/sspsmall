3482a47abfbb654e83e24c7a099357bb513a5bb5
Ben Martin <monkeyiq@users.sourceforge.net>
2024-11-09 10:42:10 +1000

    doc

--- common.sh
+++ common.sh
@@ -10,7 +10,10 @@ apacheid="apache"
 
 
 
-
+#
+# the local file is the same format as this one so you can override
+# the above settings without committing them to the main repo
+#
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 if [ -f $SCRIPT_DIR/common-local.sh ]; then
    . $SCRIPT_DIR/common-local.sh;
